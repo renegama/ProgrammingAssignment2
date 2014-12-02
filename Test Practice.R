fe1ab2eb3b420aafc752bec7f700e09c2b2c59f1
René Gallardo <renegama@users.noreply.github.com>
2014-12-02 12:19:21 -0600

    Adding an extra example

--- Test Practice.R
+++ Test Practice.R
@@ -18,6 +18,27 @@ cacheSolve(temp)
 cacheSolve(temp)
 
 #Example 2
+#Matrix that does have inverse
+mm <- matrix(c(3, 5, 1, 6, 2, 2, 3, 1, 2), ncol=3)
+
+#matrix
+mm
+
+#checking it has determinant
+abs(det(mm))
+
+#makeing
+temp <- makeCacheMatrix(mm)
+
+#calculating first time
+cacheSolve(temp)
+
+#second from cache
+cacheSolve(temp)
+
+
+
+#Example 3
 #Matrix that does not have inverse
 mm <- matrix(c(3, 5, 1, 6, 2, 2, 3, 1, 1), ncol=3)
 
